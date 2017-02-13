fb87fde75b144b92039dd44f16c11c1debe71bc9
kslr <kslrwang@gmail.com>
2017-02-13 23:39:20 +0800

    reset code

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const url = process.env.APP_URL || 'YOU_SITE_URL';
 const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
 const bodyParser = require('body-parser');
-// const request = require('request');
-// const pinyin = require('pinyin');
+const request = require('request');
+const pinyin = require('pinyin');
 
 const bot = new TelegramBot(TOKEN);
 const app = express();
@@ -18,66 +18,52 @@ bot.setWebHook(`${url}/bot${TOKEN}`);
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-    console.info(`Your ip ${req.ip}`);
-    res.sendStatus(200);
+  console.info(`Your ip ${req.ip}`);
+  res.sendStatus(200);
 });
 
 // We are receiving updates at the route below!
 app.post(`/bot${TOKEN}`, (req, res) => {
-    bot.processUpdate(req.body);
-    res.sendStatus(200);
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
 });
 
 app.listen(PORT, () => {
-    console.log(`server is listening on ${PORT}`);
+  console.log(`server is listening on ${PORT}`);
 });
 
-// bot.onText(/^\/aqi$/, (msg) => {
-//     bot.sendMessage(msg.chat.id, 'Examples: /aqi 济南|濟南|jinan');
-// });
-//
-// bot.onText(/\/aqi (.+)/, (msg, match) => {
-//     let city = match[1];
-//     const re = /[^\u4e00-\u9fa5]|[\uFE30-\uFFA0]/;
-//     if (!re.test(city)) {
-//         city = pinyin(city, {
-//             segment: false,
-//             style: pinyin.STYLE_NORMAL,
-//         });
-//         city = city.join('');
-//     }
-//
-//     console.log(`query ${city} city`);
-//     request(`http://aqicn.org/aqicn/json/android/${city}/json`, (error, response, body) => {
-//         if (!error && response.statusCode === 200) {
-//             const res = JSON.parse(body);
-//             if (res.wgt) {
-//                 console.log(res.wgt);
-//                 bot.sendPhoto(msg.chat.id, res.wgt);
-//             } else {
-//                 bot.sendMessage(msg.chat.id, `Can not find ${city} city`);
-//             }
-//         } else {
-//             console.error(error);
-//         }
-//     });
-// });
-
-bot.on('message', (msg) => {
-    if (!msg.text) {
-        bot.sendMessage(msg.chat.id, 'Please enter a text message');
-    }
-    // “private”, “group”, “supergroup” or “channel”
-
-    switch (msg.chat.type) {
-    case 'private':
-        console.info('private');
-        break;
-    default:
-        console.info('default');
-    }
+bot.onText(/^\/aqi$/, (msg) => {
+  bot.sendMessage(msg.chat.id, 'Examples: /aqi 济南|濟南|jinan');
 });
+
+bot.onText(/\/aqi (.+)/, (msg, match) => {
+  let city = match[1];
+  const re = /[^\u4e00-\u9fa5]|[\uFE30-\uFFA0]/;
+  if (!re.test(city)) {
+    city = pinyin(city, {
+      segment: false,
+      style: pinyin.STYLE_NORMAL,
+    });
+    city = city.join('');
+  }
+
+  console.log(`query ${city} city`);
+  request(`http://aqicn.org/aqicn/json/android/${city}/json`, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const res = JSON.parse(body);
+      if (res.wgt) {
+        console.log(res.wgt);
+        bot.sendPhoto(msg.chat.id, res.wgt);
+      } else {
+        bot.sendMessage(msg.chat.id, `Can not find ${city} city`);
+      }
+    } else {
+      console.error(error);
+    }
+  });
+});
+
 
 bot.on('webhook_error', (error) => {
-    console.log(error.response.body);
+  console.log(error.response.body);
 });
