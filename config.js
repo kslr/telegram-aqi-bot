e985dae918088ada65f2a6f9c1d22579f5fca5e8
kslr <kslrwang@gmail.com>
2017-02-15 02:57:07 +0800

    Use polling communication, Add novice tips

---
+++ config.js
@@ -0,0 +1,7 @@
+const config = {
+  token: 'your telegram token',
+  tips: '例: /aqi 济南  (城市名支持中文、繁体中文、拼音、英文)',
+  not_find_city_tips: '抱歉，没有找到你的城市，请检查拼写',
+}
+
+module.exports = config;
--- server.js
+++ server.js
@@ -1,83 +1,50 @@
-
-const TOKEN = process.env.TELEGRAM_TOKEN || 'YOUR_TELEGRAM_BOT_TOKEN';
-const PORT = process.env.LEANCLOUD_APP_PORT || '443';
-const url = process.env.APP_URL || 'YOU_SITE_URL';
-
+const config = require('./config');
 const TelegramBot = require('node-telegram-bot-api');
-const express = require('express');
-const bodyParser = require('body-parser');
-// const request = require('request');
-// const pinyin = require('pinyin');
+const pinyin = require('pinyin');
+const request = require('request');
 
-const bot = new TelegramBot(TOKEN);
-const app = express();
-
-bot.setWebHook(`${url}/bot${TOKEN}`);
-
-// parse the updates to JSON
-app.use(bodyParser.json());
-
-app.get('/', (req, res) => {
-    console.info(`Your ip ${req.ip}`);
-    res.sendStatus(200);
+const bot = new TelegramBot(config.token, {
+  polling: true,
+  onlyFirstMatch: true,
 });
 
-// We are receiving updates at the route below!
-app.post(`/bot${TOKEN}`, (req, res) => {
-    bot.processUpdate(req.body);
-    res.sendStatus(200);
+bot.onText(/^\/aqi$/, (msg) => {
+  bot.sendMessage(msg.chat.id, config.tips);
 });
 
-app.listen(PORT, () => {
-    console.log(`server is listening on ${PORT}`);
-});
+bot.onText(/\/aqi (.+)/, (msg, match) => {
+  let city = match[1];
+  const re = /[^\u4e00-\u9fa5]|[\uFE30-\uFFA0]/;
+  if (!re.test(city)) {
+    city = pinyin(city, {
+      segment: false,
+      style: pinyin.STYLE_NORMAL,
+    });
+    city = city.join('');
+  }
 
-// bot.onText(/^\/aqi$/, (msg) => {
-//     bot.sendMessage(msg.chat.id, 'Examples: /aqi 济南|濟南|jinan');
-// });
-//
-// bot.onText(/\/aqi (.+)/, (msg, match) => {
-//     let city = match[1];
-//     const re = /[^\u4e00-\u9fa5]|[\uFE30-\uFFA0]/;
-//     if (!re.test(city)) {
-//         city = pinyin(city, {
-//             segment: false,
-//             style: pinyin.STYLE_NORMAL,
-//         });
-//         city = city.join('');
-//     }
-//
-//     console.log(`query ${city} city`);
-//     request(`http://aqicn.org/aqicn/json/android/${city}/json`, (error, response, body) => {
-//         if (!error && response.statusCode === 200) {
-//             const res = JSON.parse(body);
-//             if (res.wgt) {
-//                 console.log(res.wgt);
-//                 bot.sendPhoto(msg.chat.id, res.wgt);
-//             } else {
-//                 bot.sendMessage(msg.chat.id, `Can not find ${city} city`);
-//             }
-//         } else {
-//             console.error(error);
-//         }
-//     });
-// });
-
-bot.on('message', (msg) => {
-    if (!msg.text) {
-        bot.sendMessage(msg.chat.id, 'Please enter a text message');
+  console.log(`query ${city} city`);
+  request(`http://aqicn.org/aqicn/json/android/${city}/json`, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const res = JSON.parse(body);
+      if (res.wgt) {
+        console.log(res.wgt);
+        bot.sendPhoto(msg.chat.id, res.wgt);
+      } else {
+        bot.sendMessage(msg.chat.id, config.not_find_city_tips);
+      }
+    } else {
+      console.error(error);
     }
-    // “private”, “group”, “supergroup” or “channel”
-
-    switch (msg.chat.type) {
-    case 'private':
-        console.info('private');
-        break;
-    default:
-        console.info('default');
-    }
+  });
 });
 
-bot.on('webhook_error', (error) => {
-    console.log(error.response.body);
+bot.onText(/(.+)/, (msg) => {
+  bot.sendMessage(msg.chat.id, config.tips);
+});
+
+
+bot.on('polling_error', (error) => {
+  console.error(error.code);
+  console.error(error.response.body);
 });
